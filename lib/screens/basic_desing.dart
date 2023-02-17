55b6081222cfc01c6728867b4405bd80a45b7658
jsemalvarez <jsemalvarez@gmail.com>
2023-02-16 22:56:54 -0300

    diseño basico - parte 2

--- lib/screens/basic_desing.dart
+++ lib/screens/basic_desing.dart
@@ -5,9 +5,49 @@ class BasicDesingScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Image(image: AssetImage('assets/landscape.jpg')),
+    return Scaffold(
+        body: Column(
+      children: const [
+        Image(image: AssetImage('assets/landscape.jpg')),
+        Title()
+      ],
+    ));
+  }
+}
+
+class Title extends StatelessWidget {
+  const Title({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: const [
+              Text(
+                'Este seria un titulo',
+                style:
+                    TextStyle(fontWeight: FontWeight.bold, color: Colors.black),
+              ),
+              Text(
+                'Este seria un sub titulo',
+                style: TextStyle(color: Colors.black45),
+              ),
+            ],
+          ),
+          Expanded(child: Container()),
+          const Icon(
+            Icons.star,
+            color: Colors.red,
+          ),
+          const Text('41')
+        ],
       ),
     );
   }
