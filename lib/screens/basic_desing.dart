4ead13b02b1de45c13776f47af39e3f0666e2018
jsemalvarez <jsemalvarez@gmail.com>
2023-02-17 00:30:54 -0300

    diseño basico - parte 3

--- lib/screens/basic_desing.dart
+++ lib/screens/basic_desing.dart
@@ -7,9 +7,15 @@ class BasicDesingScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Column(
-      children: const [
-        Image(image: AssetImage('assets/landscape.jpg')),
-        Title()
+      children: [
+        const Image(image: AssetImage('assets/landscape.jpg')),
+        const Title(),
+        const ButtonSection(),
+        Container(
+          margin: const EdgeInsets.symmetric(horizontal: 20),
+          child: const Text(
+              "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."),
+        )
       ],
     ));
   }
@@ -49,6 +55,63 @@ class Title extends StatelessWidget {
           const Text('41')
         ],
       ),
+    );
+  }
+}
+
+class ButtonSection extends StatelessWidget {
+  const ButtonSection({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: const [
+          CustomButton(
+            icon: Icons.phone,
+            text: 'SHARE',
+          ),
+          CustomButton(
+            icon: Icons.send,
+            text: 'SEND',
+          ),
+          CustomButton(
+            icon: Icons.share,
+            text: 'SHARE',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  final IconData icon;
+  final String text;
+  const CustomButton({
+    Key? key,
+    required this.icon,
+    required this.text,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Icon(
+          icon,
+          color: Colors.blue,
+        ),
+        const SizedBox(height: 5),
+        Text(
+          text,
+          style: const TextStyle(color: Colors.blue),
+        )
+      ],
     );
   }
 }
